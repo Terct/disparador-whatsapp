42d369ab36c729dd298032286a7e609d6d2914eb
Terct <servercentralterct@gmail.com>
2023-10-20 12:02:55 -0300

    fist commit

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const multer = require('multer');
-const xlsx = require('xlsx'); 
+const xlsx = require('xlsx');
+const { url } = require('inspector');
 
 
 const app = express();
@@ -19,7 +20,7 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
@@ -66,13 +67,15 @@ app.post('/upload', upload.single('file'), (req, res) => {
 });
 
 
+const Myurl = "https://n8n.dagestao.com/webhook/startorstop"
+
 // Rota para iniciar alguma ação
 app.get('/start', (req, res) => {
     // Define o parâmetro "type" como "start"
     const params = { type: 'start' };
 
     // Faz uma requisição GET para o serviço externo
-    axios.get('https://n8n.dagestao.com/webhook-test/startorstop', { params })
+    axios.get(Myurl, { params })
         .then(response => {
             // Se a ação for bem-sucedida, você pode enviar uma resposta 200 (OK).
             res.sendStatus(200);
@@ -84,13 +87,32 @@ app.get('/start', (req, res) => {
         });
 });
 
+// Rota para iniciar alguma ação
+app.get('/restart', (req, res) => {
+    // Define o parâmetro "type" como "start"
+    const params = { type: 'restart' };
+
+    // Faz uma requisição GET para o serviço externo
+    axios.get(Myurl, { params })
+        .then(response => {
+            // Se a ação for bem-sucedida, você pode enviar uma resposta 200 (OK).
+            res.sendStatus(200);
+        })
+        .catch(error => {
+            console.error('Erro ao fazer a requisição "start":', error);
+            // Em caso de erro, você pode enviar uma resposta de erro, como 500 (Internal Server Error).
+            res.sendStatus(500);
+        });
+});
+
+
 // Rota para parar alguma ação
 app.get('/stop', (req, res) => {
     // Define o parâmetro "type" como "stop"
     const params = { type: 'stop' };
 
     // Faz uma requisição GET para o serviço externo
-    axios.get('https://n8n.dagestao.com/webhook-test/startorstop', { params })
+    axios.get(Myurl, { params })
         .then(response => {
             // Se a ação for bem-sucedida, você pode enviar uma resposta 200 (OK).
             res.sendStatus(200);
@@ -104,7 +126,7 @@ app.get('/stop', (req, res) => {
 
 app.get('/search-status', (req, res) => {
     const statusFilePath = path.join(__dirname, 'src', 'status.json');
-    
+
     fs.readFile(statusFilePath, 'utf8', (err, data) => {
         if (err) {
             console.error('Erro ao ler o arquivo status.json:', err);
@@ -122,6 +144,128 @@ app.get('/search-status', (req, res) => {
 });
 
 
+app.get('/search-progress', (req, res) => {
+    const statusFilePath = path.join(__dirname, 'src', 'status.json');
+
+    fs.readFile(statusFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler o arquivo status.json:', err);
+            res.status(500).json({ error: 'Erro ao buscar o status' });
+        } else {
+            try {
+                const statusData = JSON.parse(data);
+                res.json(statusData);
+            } catch (error) {
+                console.error('Erro ao analisar o JSON do arquivo status.json:', error);
+                res.status(500).json({ error: 'Erro ao buscar o status' });
+            }
+        }
+    });
+});
+
+
+app.get('/set-status', (req, res) => {
+    const newValue = req.query.value; // Obtém o valor do parâmetro "value" na URL.
+
+    // Valide o novo valor, certificando-se de que atende aos requisitos desejados.
+    if (newValue === 'started' || newValue === 'stopped' || newValue === 'processing') {
+        // Leia o arquivo JSON existente.
+        const statusFilePath = path.join(__dirname, 'src', 'status.json');
+        const existingData = require(statusFilePath);
+
+        // Atualize o valor de "status" no objeto JSON.
+        existingData[0].status = newValue;
+
+        // Salve o objeto JSON atualizado de volta no arquivo.
+        fs.writeFileSync(statusFilePath, JSON.stringify(existingData, null, 2));
+
+        res.json({ success: true, message: 'Status atualizado com sucesso.' });
+    } else {
+        res.status(400).json({ error: 'O valor "value" deve ser "started" ou "stopped".' });
+    }
+});
+
+
+app.get('/set-line', (req, res) => {
+    const newValue = req.query.value;
+
+    // Certifique-se de validar o novo valor aqui, se necessário.
+
+    // Converta a string em um número (inteiro neste caso).
+    const numericValue = parseInt(newValue, 10);
+
+    if (!isNaN(numericValue)) {
+        // Leia o arquivo JSON existente.
+        const statusFilePath = path.join(__dirname, 'src', 'status.json');
+        const existingData = require(statusFilePath);
+
+        // Atualize o valor de "lastLine" no objeto JSON com o valor numérico.
+        existingData[0].lastLine = numericValue;
+
+        // Salve o objeto JSON atualizado de volta no arquivo.
+        fs.writeFileSync(statusFilePath, JSON.stringify(existingData, null, 2));
+
+        res.json({ success: true, message: 'Linha atualizado com sucesso.' });
+    } else {
+        res.json({ success: false, message: 'O valor não é um número válido.' });
+    }
+});
+
+
+app.get('/set-progress', (req, res) => {
+    const newValue = req.query.value; // Obtém o valor do parâmetro "value" na URL.
+
+
+    // Leia o arquivo JSON existente.
+    const statusFilePath = path.join(__dirname, 'src', 'status.json');
+    const existingData = require(statusFilePath);
+
+    // Atualize o valor de "status" no objeto JSON.
+    existingData[0].progress = newValue;
+
+    // Salve o objeto JSON atualizado de volta no arquivo.
+    fs.writeFileSync(statusFilePath, JSON.stringify(existingData, null, 2));
+
+    res.json({ success: true, message: 'Status atualizado com sucesso.' });
+
+});
+
+app.get('/fetch-instances', async (req, res) => {
+    const maxLine = req.query.maxline;
+    const evo = req.query.evo;
+    const apiKey = req.query.apikey;
+
+    if (!maxLine || isNaN(maxLine)) {
+        return res.status(400).json({ error: 'O parâmetro "maxline" deve ser um número válido.' });
+    }
+
+    const instances = [];
+
+    // Faz um loop para criar as requisições e obter as instâncias.
+    for (let i = 1; i <= maxLine; i++) {
+        const instanceName = `Line${i}`;
+        const url = `${evo}/instance/connectionState/${instanceName}`;
+
+        try {
+            const response = await axios.get(url, {
+                headers: {
+                    apikey: apiKey,
+                },
+            });
+
+            const instanceData = response.data;
+            if (instanceData.instance && instanceData.instance.state === 'open') {
+                instances.push({ line: i })
+            }
+        } catch (error) {
+            console.error(`Erro ao obter instância ${instanceName}:`, error);
+        }
+    }
+
+    res.json(instances);
+});
+
+
 app.listen(port, () => {
     console.log(`API está rodando em http://localhost:${port}`);
-  });
+});
